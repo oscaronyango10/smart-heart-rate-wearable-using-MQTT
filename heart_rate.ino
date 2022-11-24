f42546eb78261e497042dbde4ebe2a2822586093
Oscar Onyango <74372181+oscaronyango10@users.noreply.github.com>
2022-11-24 12:58:10 +0300

    Add files via upload

---
+++ heart_rate.ino
@@ -0,0 +1,231 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266WebServer.h>
+#include "MAX30100_PulseOximeter.h"
+const char* ssid = "Amour";
+const char* password = "engineer@1";
+const char* mqtt_server = "driver.cloudmqtt.com";
+const char *mqtt_user = "kmsmamvp"; 
+const char *mqtt_pass = "88AdSsQvLTk8";
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
+
+#define REPORTING_PERIOD_MS   3000
+ESP8266WebServer server(80);
+float BPM, SpO2;
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+  delay(100);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros()); 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", handle_OnConnect);
+  server.onNotFound(handle_NotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+  Serial.print("Initializing pulse oximeter..");
+  if (!pox.begin()) {
+    Serial.println("FAILED");
+    for (;;);
+  } else {
+    Serial.println("SUCCESS");
+    
+  }
+
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() { 
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+         String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+        if (client.connect(clientId.c_str(), mqtt_user, mqtt_pass)) {
+      Serial.println("connected");
+         client.publish("outTopic", "hello world");
+    
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds"); 
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 18728);
+  client.setCallback(callback);
+  reconnect();
+}
+
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  client.loop();
+  server.handleClient();
+  pox.update();
+  if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+
+    BPM = pox.getHeartRate();
+    SpO2 = pox.getSpO2();
+    _pulseOximeterFunction(BPM, SpO2);
+    Serial.print("BPM: ");
+    Serial.println(BPM);
+
+    Serial.print("SpO2: ");
+    Serial.print(SpO2);
+    Serial.println("%");
+
+    Serial.println("*********************************");
+    Serial.println();
+    tsLastReport = millis();
+  }
+
+}
+
+// Set up the PPulseometer to work
+void _pulseOximeterFunction(float BPM, float SpO2 ){
+  delay(1000);
+//BPM
+  Serial.print("Publish message: ");
+  Serial.println(BPM);
+
+   int numBPM = (int)BPM;
+  char cstrBPM[16];
+  itoa(numBPM, cstrBPM, 10);
+
+//Sp02
+  int numSpO2 = (int)SpO2;
+   char cstrSpO2[16];
+   itoa(numSpO2, cstrSpO2, 10);
+
+  delay(1500);
+  client.publish("bmp", cstrBPM);
+  client.publish("sp01", cstrSpO2);
+}
+
+// Connectivity functions
+void handle_OnConnect() {
+  server.send(200, "text/html", SendHTML(BPM, SpO2));
+}
+
+void handle_NotFound() {
+  server.send(404, "text/plain", "Not found");
+}
+
+
+// format and send the data recorded from the oximeter
+
+String SendHTML(float BPM, float SpO2) {
+  String ptr = "<!DOCTYPE html>";
+  ptr += "<html>";
+  ptr += "<head>";
+  ptr += "<title>ESP8266 WebServer</title>";
+  ptr += "<meta name='viewport' content='width=device-width, initial-scale=1.0'>";
+  ptr += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.2/css/all.min.css'>";
+  ptr += "<link rel='stylesheet' type='text/css' href='styles.css'>";
+  ptr += "<style>";
+  ptr += "body { background-color: #fff; font-family: sans-serif; color: #333333; font: 14px Helvetica, sans-serif box-sizing: border-box;}";
+  ptr += "#page { margin: 20px; background-color: #fff;}";
+  ptr += ".container { height: inherit; padding-bottom: 20px;}";
+  ptr += ".header { padding: 20px;}";
+  ptr += ".header h1 { padding-bottom: 0.3em; color: #008080; font-size: 45px; font-weight: bold; font-family: Garmond, 'sans-serif'; text-align: center;}";
+  ptr += "h2 { padding-bottom: 0.2em; border-bottom: 1px solid #eee; margin: 2px; text-align: left;}";
+  ptr += ".header h3 { font-weight: bold; font-family: Arial, 'sans-serif'; font-size: 17px; color: #b6b6b6; text-align: center;}";
+  ptr += ".box-full { padding: 20px; border 1px solid #ddd; border-radius: 1em 1em 1em 1em; box-shadow: 1px 7px 7px 1px rgba(0,0,0,0.4); background: #fff; margin: 20px; width: 300px;}";
+  ptr += "@media (max-width: 494px) { #page { width: inherit; margin: 5px auto; } #content { padding: 1px;} .box-full { margin: 8px 8px 12px 8px; padding: 10px; width: inherit;; float: none; } }";
+  ptr += "@media (min-width: 494px) and (max-width: 980px) { #page { width: 465px; margin 0 auto; } .box-full { width: 380px; } }";
+  ptr += "@media (min-width: 980px) { #page { width: 930px; margin: auto; } }";
+  ptr += ".sensor { margin: 12px 0px; font-size: 2.5rem;}";
+  ptr += ".sensor-labels { font-size: 1rem; vertical-align: middle; padding-bottom: 15px;}";
+  ptr += ".units { font-size: 1.2rem;}";
+  ptr += "hr { height: 1px; color: #eee; background-color: #eee; border: none;}";
+  ptr += "</style>";
+
+  //Ajax Code Start
+  ptr += "<script>\n";
+  ptr += "setInterval(loadDoc,1000);\n";
+  ptr += "function loadDoc() {\n";
+  ptr += "var xhttp = new XMLHttpRequest();\n";
+  ptr += "xhttp.onreadystatechange = function() {\n";
+  ptr += "if (this.readyState == 4 && this.status == 200) {\n";
+  ptr += "document.body.innerHTML =this.responseText}\n";
+  ptr += "};\n";
+  ptr += "xhttp.open(\"GET\", \"/\", true);\n";
+  ptr += "xhttp.send();\n";
+  ptr += "}\n";
+  ptr += "</script>\n";
+  //Ajax Code END
+
+  ptr += "</head>";
+  ptr += "<body>";
+  ptr += "<div id='page'>";
+  ptr += "<div class='header'>";
+  ptr += "<h1>MAX30100 ESP8266 WebServer</h1>";
+  ptr += "<h3><a href='https://theiotprojects.com'>https://theiotprojects.com</a></h3>";
+  ptr += "</div>";
+  ptr += "<div id='content' align='center'>";
+  ptr += "<div class='box-full' align='left'>";
+  ptr += "<h2>Sensor Readings</h2>";
+  ptr += "<div class='sensors-container'>";
+
+  //For Heart Rate
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-heartbeat' style='color:#cc3300'></i>";
+  ptr += "<span class='sensor-labels'> Heart Rate </span>";
+  ptr += (int)BPM;
+  ptr += "<sup class='units'>BPM</sup>";
+  ptr += "</p>";
+  ptr += "<hr>";
+
+  //For Sp02
+  ptr += "<p class='sensor'>";
+  ptr += "<i class='fas fa-burn' style='color:#f7347a'></i>";
+  ptr += "<span class='sensor-labels'> Sp02 </span>";
+  ptr += (int)SpO2;
+  ptr += "<sup class='units'>%</sup>";
+  ptr += "</p>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</div>";
+  ptr += "</body>";
+  ptr += "</html>";
+  return ptr;
+}
